cea5b927134bb87ae7e228d1cedb22dc477bb35e
money <manmohit.singh@gmail.com>
2020-04-24 21:10:58 -0700

    Refactor code for organization.
    Do Not Repeat (DRY) Principle for moving headers into header.php file.

--- index.php
+++ index.php
@@ -1,11 +1,19 @@
 <!DOCTYPE html>
 <html>
-<title>HTML Tutorial</title>
-<body>
-    <h1>Hello world!</h1>
-    <?php
-      $db_connection = pg_connect("host=localhost dbname=ring_development user=application");
-    ?>
+  <?php include 'views/header.php' ?>
 
-</body>
+  <title>HTML Tutorial</title>
+  <body class="container-fluid">
+      <h1>Hello world!</h1>
+
+      <div class="card" style="width: 18rem;">
+        <img src="../../assets/images/sky.jpg" alt="" class="img-fluid">
+        <div class="card-body">
+          <a href="views/search.php" class="btn btn-primary">Search Businesses</a>
+        </div>
+      </div>
+      <?php
+        $db_connection = pg_connect("host=localhost dbname=ring_development user=application");
+      ?>
+  </body>
 </html>
