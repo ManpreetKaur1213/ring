199615ab49899d3b9a4c441c45bbce444015c8a8
ManpreetKaur1213 <63481510+ManpreetKaur1213@users.noreply.github.com>
2020-05-25 18:09:18 +1200

    added file curl.php and modified file search.php, used curl function to set authorization under header in curl.php and print results using json decode in file search.php

---
+++ views/curl.php
@@ -0,0 +1,43 @@
+
+<?php
+  $result = NULL;
+
+	if (isset($_GET['term'], $_GET['location'])) {
+		$term = $_GET['term'];
+		$location = $_GET['location'];
+		$query = http_build_query(array('term' =>$term , 'location'=>$location));
+
+/*$city_names = array(
+    'delhi',
+    'mumbai',
+    'kolkata',
+    'chennai'
+);
+$city_query = http_build_query(array('city' => $city_names),());*/
+
+
+
+	$api_url="https://api.yelp.com/v3/businesses/search?".$query;
+
+	// echo $api_url;
+	
+
+	$api_secret="M-UrVIi40m-K31av-glzYU0GAW6z58ZSdDAq3FYX4npIotIofOfcsSiudwY-nnkVNERUj9DIw5nAqNqYbsMdq5gh9Wtynmd3Wyyh4KaBafxZypfWksHuST9V-0CiXnYx";
+
+	$ch = curl_init();
+
+  curl_setopt($ch, CURLOPT_URL, $api_url);
+
+ curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
+
+ curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE); 
+
+ curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json' , 'Authorization: bearer M-UrVIi40m-K31av-glzYU0GAW6z58ZSdDAq3FYX4npIotIofOfcsSiudwY-nnkVNERUj9DIw5nAqNqYbsMdq5gh9Wtynmd3Wyyh4KaBafxZypfWksHuST9V-0CiXnYx'));
+
+ 		$result = curl_exec($ch);
+ 
+
+ 		curl_close($ch);
+	};
+
+?>
---
+++ views/search.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+ <html>
+   <?php include 'header.php' ?>
+   <?php include_once 'curl.php' ?>
+
+   <body class="container" style="background-image: url('../assets/images/sky.jpg');background-repeat: no-repeat;background-size: cover;">
+
+     <form class="col-6 offset-3 mt-5 bg-light rounded shadow p-3" method="GET">
+       <div class="form-row">
+         <div class="form-group col-6">
+           <label for="term" class="sr-only">Term</label>
+           <input type="text" class="form-control" name="term" placeholder="Search yelp for business" required=""/>
+         </div>
+         <div class="form-group col-6">
+           <label for="location" class="sr-only">Another label</label>
+           <input type="text" class="form-control" name="location" placeholder="business location" required=""/>
+         </div>
+       </div>
+
+       <input type="submit" class="btn btn-primary btn-block" value="Submit"/>
+     </form>
+     <?php
+
+
+     if ($result != NULL) {
+
+    $data=json_decode($result,true);
+    foreach($data['businesses'] as $business){
+        echo $business['name']."</br>";
+        echo "<img src=".$business['image_url']." />";
+        echo "<hr class='col-12'>";
+      }
+     }
+     ?>
+ 
+
+
+   </body>
+ </html> 
