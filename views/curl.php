65c18a414cd499a4280bb2f37d6a70c4c19f39ac
ManpreetKaur1213 <63481510+ManpreetKaur1213@users.noreply.github.com>
2020-06-24 09:47:32 +1200

    [wip] draft

--- views/curl.php
+++ views/curl.php
@@ -5,37 +5,42 @@
 	if (isset($_GET['term'], $_GET['location'])) {
 		$term = $_GET['term'];
 		$location = $_GET['location'];
-		$query = http_build_query(array('term' =>$term , 'location'=>$location));
+		$total = 11;
+		$limit = 2;
 
-/*$city_names = array(
-    'delhi',
-    'mumbai',
-    'kolkata',
-    'chennai'
-);
-$city_query = http_build_query(array('city' => $city_names),());*/
+		if (isset($_GET['currentpage'])) {
+            $currentpage = intval($_GET['currentpage']);
+        } else {
+            $currentpage = 1;
+        }
+        
+    $offset = ($currentpage-1) * $limit;
+        
+    $total_pages = ceil($total / $limit);
+    
+    $query = http_build_query(array('term' =>$term , 'location'=>$location , 'offset'=>$offset,'limit'=>$limit));
 
 
+	  $api_url="https://api.yelp.com/v3/businesses/search?".$query;
 
-	$api_url="https://api.yelp.com/v3/businesses/search?".$query;
-
-	// echo $api_url;
-	
-
-	$api_secret="M-UrVIi40m-K31av-glzYU0GAW6z58ZSdDAq3FYX4npIotIofOfcsSiudwY-nnkVNERUj9DIw5nAqNqYbsMdq5gh9Wtynmd3Wyyh4KaBafxZypfWksHuST9V-0CiXnYx";
+      $api_secret="M-UrVIi40m-K31av-glzYU0GAW6z58ZSdDAq3FYX4npIotIofOfcsSiudwY-nnkVNERUj9DIw5nAqNqYbsMdq5gh9Wtynmd3Wyyh4KaBafxZypfWksHuST9V-0CiXnYx";
 
 	$ch = curl_init();
 
-  curl_setopt($ch, CURLOPT_URL, $api_url);
+    curl_setopt($ch, CURLOPT_URL, $api_url);
 
- curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
 
- curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE); 
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE); 
 
- curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json' , 'Authorization: bearer M-UrVIi40m-K31av-glzYU0GAW6z58ZSdDAq3FYX4npIotIofOfcsSiudwY-nnkVNERUj9DIw5nAqNqYbsMdq5gh9Wtynmd3Wyyh4KaBafxZypfWksHuST9V-0CiXnYx'));
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json' , 'Authorization: bearer M-UrVIi40m-K31av-glzYU0GAW6z58ZSdDAq3FYX4npIotIofOfcsSiudwY-nnkVNERUj9DIw5nAqNqYbsMdq5gh9Wtynmd3Wyyh4KaBafxZypfWksHuST9V-0CiXnYx'));
 
  		$result = curl_exec($ch);
+ 		$data=json_decode($result, true);
  
+ 		for ($i=1; $i < $total_pages; $i++) { 
+ 			echo "<a href=/views/search.php?term=".$_GET['term']."&location=".$_GET['location']."&currentpage=".$i.">".$i."</a>";
+ 		};
 
  		curl_close($ch);
 	};
--- views/search.php
+++ views/search.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<<<<<<< HEAD
+
  <html>
    <?php include 'header.php' ?>
    <?php include_once 'curl.php' ?>
@@ -25,43 +25,14 @@
 
      if ($result != NULL) {
 
-    $data=json_decode($result,true);
+    
     foreach($data['businesses'] as $business){
         echo $business['name']."</br>";
-        echo "<img src=".$business['image_url']." />";
-        echo "<hr class='col-12'>";
+        echo "<img src=".$business['image_url']." />"."</br>";
       }
      }
+
      ?>
- 
-
-
-   </body>
- </html> 
-=======
-<html>
-  <?php include 'header.php' ?>
-
-  <body class="container" style="background-image: url('../assets/images/sky.jpg');background-repeat: no-repeat;background-size: cover;">
-    
-    <form class="col-6 offset-3 mt-5 bg-light rounded shadow p-3">
-      <div class="form-row">
-        <div class="form-group col-6">
-          <label for="term" class="sr-only">Term</label>
-          <input type="text" class="form-control" id="term" placeholder="Search yelp for business" required="">
-        </div>
-        <div class="form-group col-6">
-          <label for="location" class="sr-only">Another label</label>
-          <input type="text" class="form-control" id="location" placeholder="business location" required="">
-        </div>
-      </div>
-      
-      <button type="submit" class="btn btn-primary btn-block">Search</button>
-    </form>
   
-</body>
-    
-    
-  </body>
+ </body>
 </html>
->>>>>>> 21a398e4bff2ef70f32d20e1af8cf8353593fdd5
