f821029102bd5cdb6328e112f220614ddb64f849
ManpreetKaur1213 <63481510+ManpreetKaur1213@users.noreply.github.com>
2020-07-06 14:26:25 +1200

    Remove empty line

--- index.php
+++ index.php
@@ -13,7 +13,6 @@
         </div>
       </div>
       <?php
-      
           $db_connection = pg_connect("host=".getenv('DB_HOST')." dbname=".getenv('DB_NAME')." user=".getenv('DB_USERNAME'));
       ?>
   </body>
